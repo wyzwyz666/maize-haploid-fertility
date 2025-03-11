7fba5559f2ffcef6d2febf3fe81e06b8e4407f10
wyzwyz666 <100083927+wyzwyz666@users.noreply.github.com>
2025-03-11 21:38:59 +0800

    Update utils_fit.py

--- sourcecode/utils/utils_fit.py
+++ sourcecode/utils/utils_fit.py
@@ -140,8 +140,7 @@ def fit_one_epoch(backbone, model_train, model, loss_history, optimizer, epoch, 
         #   保存权值
         #-----------------------------------------------#
         if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
-            if val_accuracy / epoch_step_val > 0.77:
-                torch.save(model.state_dict(), os.path.join(save_dir,f"ep{epoch + 1}-train_acc{total_accuracy / epoch_step:.3f}-val_acc{val_accuracy / epoch_step_val:.3f}-{backbone}.pth"))
+            torch.save(model.state_dict(), os.path.join(save_dir,f"ep{epoch + 1}-train_acc{total_accuracy / epoch_step:.3f}-val_acc{val_accuracy / epoch_step_val:.3f}-{backbone}.pth"))
         if len(loss_history.val_loss) <= 1 or (val_loss / epoch_step_val) <= min(loss_history.val_loss):
             print('Save best model to best_epoch_weights.pth')
             # torch.save(model.state_dict(), os.path.join(save_dir, f"best_epoch_weights-{backbone}.pth"))
