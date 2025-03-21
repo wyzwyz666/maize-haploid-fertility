d8a05d77e08efc1b8f321f40ba78e8e976e448aa
wyzwyz666 <100083927+wyzwyz666@users.noreply.github.com>
2025-03-21 15:09:51 +0800

    Update dataloader.py

--- sourcecode/utils/dataloader.py
+++ sourcecode/utils/dataloader.py
@@ -27,9 +27,7 @@ class DataGenerator(data.Dataset):
     def __getitem__(self, index):
         annotation_path = self.annotation_lines[index].split(';')[1].split()[0]
         image = Image.open(annotation_path)
-        #------------------------------#
-        #   读取图像并转换成RGB图像
-        #------------------------------#
+        # Read image and convert to RGB image
         image   = cvtColor(image)
         if self.autoaugment_flag:
             image = self.AutoAugment(image, random=self.random)
@@ -44,9 +42,7 @@ class DataGenerator(data.Dataset):
         return np.random.rand()*(b-a) + a
 
     def get_random_data(self, image, input_shape, jitter=.3, hue=.1, sat=1.5, val=1.5, random=True):
-        #------------------------------#
-        #   获得图像的高宽与目标高宽
-        #------------------------------#
+        #   Get the height and width of the image and the height and width of the target
         iw, ih  = image.size
         h, w    = input_shape
 
@@ -56,10 +52,8 @@ class DataGenerator(data.Dataset):
             nh = int(ih*scale)
             dx = (w-nw)//2
             dy = (h-nh)//2
-
-            #---------------------------------#
-            #   将图像多余的部分加上灰条
-            #---------------------------------#
+            
+            #   Add a gray bar to the redundant part of the image
             image       = image.resize((nw,nh), Image.BICUBIC)
             new_image   = Image.new('RGB', (w,h), (128,128,128))
             new_image.paste(image, (dx, dy))
@@ -67,9 +61,7 @@ class DataGenerator(data.Dataset):
 
             return image_data
 
-        #------------------------------------------#
-        #   对图像进行缩放并且进行长和宽的扭曲
-        #------------------------------------------#
+        #   Scale the image and distort its length and width
         new_ar = iw/ih * self.rand(1-jitter,1+jitter) / self.rand(1-jitter,1+jitter)
         scale = self.rand(.75, 1.5)
         if new_ar < 1:
@@ -80,18 +72,14 @@ class DataGenerator(data.Dataset):
             nh = int(nw/new_ar)
         image = image.resize((nw,nh), Image.BICUBIC)
 
-        #------------------------------------------#
-        #   将图像多余的部分加上灰条
-        #------------------------------------------#
+        #   Add a gray bar to the redundant part of the image
         dx = int(self.rand(0, w-nw))
         dy = int(self.rand(0, h-nh))
         new_image = Image.new('RGB', (w,h), (128,128,128))
         new_image.paste(image, (dx, dy))
         image = new_image
 
-        #------------------------------------------#
-        #   翻转图像
-        #------------------------------------------#
+        #   Flip image
         flip = self.rand()<.5
         if flip: image = image.transpose(Image.FLIP_LEFT_RIGHT)
         
@@ -103,19 +91,15 @@ class DataGenerator(data.Dataset):
             image = cv2.warpAffine(np.array(image), M, (w,h), borderValue=[128, 128, 128]) 
 
         image_data      = np.array(image, np.uint8)
-        #---------------------------------#
-        #   对图像进行色域变换
-        #   计算色域变换的参数
-        #---------------------------------#
+        
+        # Color Gamut Transformation of image
+        # Calculate the parameters of Gamut Transformation
         r               = np.random.uniform(-1, 1, 3) * [hue, sat, val] + 1
-        #---------------------------------#
-        #   将图像转到HSV上
-        #---------------------------------#
+
+        #   Transfer image to HSV
         hue, sat, val   = cv2.split(cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV))
         dtype           = image_data.dtype
-        #---------------------------------#
-        #   应用变换
-        #---------------------------------#
+
         x       = np.arange(0, 256, dtype=r.dtype)
         lut_hue = ((x * r[0]) % 180).astype(dtype)
         lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
@@ -131,20 +115,14 @@ class DataGenerator(data.Dataset):
             image = self.center_crop(image)
             return image
 
-        #------------------------------------------#
-        #   resize并且随即裁剪
-        #------------------------------------------#
+        #   resize and crop
         image = self.resize_crop(image)
         
-        #------------------------------------------#
-        #   翻转图像
-        #------------------------------------------#
+        #   flip image
         flip = self.rand()<.5
         if flip: image = image.transpose(Image.FLIP_LEFT_RIGHT)
         
-        #------------------------------------------#
-        #   随机增强
-        #------------------------------------------#
+        #   random aug
         image = self.policy(image)
         return image
             
